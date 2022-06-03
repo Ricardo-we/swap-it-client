19c19fc005825091f4651de75de7df1d8af21ba3
Ricardo-we <totgamer75@gmail.com>
2022-06-02 22:45:54 -0600

    First deploy

--- src/form-validations/Login.ts
+++ src/form-validations/Login.ts
@@ -1,14 +1,14 @@
 import * as yup from 'yup';
 
 export const LoginSchema = yup.object({
-    username: yup.string().max(150).min(3),
-    password: yup.string().max(150).min(8).matches(/(?=.*[&#\$%\-_])[\s\w&#\$%\-_]{8,150}/g),
+    username: yup.string().max(150).min(3).matches(/[\w\d_-]{3,150}/g, "Username does not match requested pattern"),
+    password: yup.string().max(150).min(8).matches(/(?=.*[&#$%\-_])[\s\w&#$%\-_]{8,150}/g),
 })
 
 export const SignUpSchema = yup.object({
     username: yup.string().max(150).min(3),
-    password: yup.string().max(150).min(8).matches(/(?=.*[&#\$%\-_])[\s\w&#\$%\-_]{8,150}/g),
+    password: yup.string().max(150).min(8).matches(/(?=.*[&#$%\-_])[\s\w&#$%\-_]{8,150}/g),
     email: yup.string().email(),
-    phone_number: yup.string().matches(/[0-9+]/),
+    phone_number: yup.string().notRequired().min(8, "Min numbers: 8").max(20, "Max numbers: 20").matches(/[0-9+]/g, "Phone number does not match requested pattern"),
     location: yup.string()
 })
--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -3,15 +3,15 @@ import { AppContext } from "../../App";
 import FormControl from "../../components/FormControl";
 import CountriesSelect from "../../components/CountriesSelect";
 import { LoginSchema, SignUpSchema } from './../../form-validations/Login';
-import { toast , ToastContainer} from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import { loginRequest, signUpRequest, confirmationCodeRequest } from './../../services/users-requests';
 import NavBar from './../../components/NavBar';
 import { useNavigate } from 'react-router-dom';
 import CurrencySelect from './../../components/CurrencySelect';
 
 interface LoginForm {
-    username: string
-    password: string
+    username?: string
+    password?: string
     email?: string
     phone_number?: string
     location?: string
@@ -22,7 +22,7 @@ function Login() {
     const { setStoredUser } = useContext<any>(AppContext);
     const [signUpOpen, setSignUpOpen] = useState(false);
     const [verificationCodeConfirmation, setVerificationCodeConfirmation] = useState(false);
-    const [formData, setFormData] = useState<LoginForm>({username: '', password:''});
+    const [formData, setFormData] = useState<LoginForm>();
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         setFormData(prev => ({
@@ -31,122 +31,121 @@ function Login() {
         }))
     }
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-    }
+    const handleSubmit = (e: any) => e.preventDefault()
 
     const submitVerificationCode = async (e: any) => {
-        try{
+        try {
             e.preventDefault();
             await SignUpSchema.validate(formData);
             const response = await confirmationCodeRequest(e.target[0].value, formData);
             setVerificationCodeConfirmation(true);
             setStoredUser(response);
             navigate('/account')
-        } catch(error: any){
+        } catch (error: any) {
             toast.error(error.toString());
         }
     }
 
     const login = async () => {
-        try{
+        try {
             await LoginSchema.validate(formData);
             const response = await loginRequest(formData);
             setStoredUser(response);
             navigate('/account')
-        } catch(error: any){
+        } catch (error: any) {
             toast.error(error.toString());
         }
     }
 
     const signUp = async () => {
-        try{
+        try {
+            if (formData?.phone_number === '') formData.phone_number = undefined;
             await SignUpSchema.validate(formData);
             const response = await signUpRequest(formData);
             setVerificationCodeConfirmation(true);
 
-        } catch(error: any){
+        } catch (error: any) {
             toast.error(error.toString());
         }
     }
 
-
-    if(!verificationCodeConfirmation) return ( <>
-        <NavBar/>
-        <div className="container-sm w-50" style={{minWidth: 180}}>
-            <ToastContainer/>
-            <form className="form" onSubmit={handleSubmit}>
-                <h3>{signUpOpen ? 'Sign up!' : 'Login'}</h3>
-                <FormControl 
-                    onChange={handleChange} 
-                    name="username" 
-                    required
-                    alertMessage="Username is at least 3 characters long and should not have white spaces"
-                    pattern="^.\S{3,150}$"
-                    placeholder="Username"
-                />
-                <FormControl 
-                    pattern="^(?=.*[&#\$%\-_])[\S\w&#\$%\-_]{8,150}$" 
-                    name="password"
-                    required
-                    alertMessage="Password requires at least 8 characters and contain a special character: -_#$& and not whitespaces" 
-                    placeholder="Password" 
-                    type="password" 
-                    onChange={handleChange}
-                />
-                <FormControl 
-                    name="email"
-                    pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$" 
-                    alertMessage="Invalid email"
-                    hidden={!signUpOpen} 
-                    required={signUpOpen}
-                    type="email" 
-                    placeholder="Email" 
-                    onChange={handleChange}
-                />
-                <FormControl 
-                    onChange={handleChange} 
-                    name="phone_number"
-                    hidden={!signUpOpen} 
-                    pattern="^[\d\+\s]{5,20}$"
-                    variant="warning"
-                    alertMessage="Invalid phone number (optional field)"
-                    type="tel" 
-                    placeholder="Phone number"
-                />
-                <CountriesSelect name="location" onChange={handleChange} hidden={!signUpOpen} />
-                <CurrencySelect name="user_currency" onChange={handleChange} hidden={!signUpOpen} />
-                <button className="btn btn-link text-left" type="button" onClick={() => setSignUpOpen(prev => !prev)}>
-                    {signUpOpen ? 'Already registered login!' : 'Not an account yet? sign up!'}
-                </button>
-                {signUpOpen ?
-                    <button className="btn btn-outline-primary w-100" onClick={signUp}>
-                        Sign up 
-                    </button>
-                    : 
-                    <button className="btn btn-info w-100" onClick={login}>
-                        Login 
-                    </button>
-                }
-            </form> 
-        </div>
-        </>
-    );
-    return (
-        <>
-        <NavBar/>
-            <div className="container-sm w-50" style={{minWidth: 180}}>
+    return (<>
+        <NavBar />
+        {verificationCodeConfirmation ?
+            <div className="container-sm w-50" style={{ minWidth: 180 }}>
                 <form onSubmit={submitVerificationCode} className="form">
                     <h3>Enter your verification code it was sent to your email</h3>
-                    <ToastContainer/>
+                    <ToastContainer />
                     <FormControl placeholder="Enter verification code" pattern="^[0-9]{4,}$" />
                     <button className="btn btn-outline-primary w-100" type="submit">
                         Confirm
                     </button>
+                    <button className="btn btn-outline-info w-100" onClick={() => setVerificationCodeConfirmation(false)}>
+                        Change some fields
+                    </button>
                 </form>
             </div>
-        </>
-    )
+            :
+            <div className="container-sm w-50" style={{ minWidth: 180 }}>
+                <ToastContainer />
+                <form className="form" onSubmit={handleSubmit}>
+                    <h3>{signUpOpen ? 'Sign up!' : 'Login'}</h3>
+                    <FormControl
+                        onChange={handleChange}
+                        name="username"
+                        required
+                        alertMessage="Username is at least 3 characters long and should not have white spaces and"
+                        pattern="^.\S{3,150}$"
+                        placeholder="Username"
+                    />
+                    <FormControl
+                        pattern="^(?=.*[&#\$%\-_])[\S\w&#\$%\-_]{8,150}$"
+                        name="password"
+                        required
+                        alertMessage="Password requires at least 8 characters and contain a special character: -_#$& and not whitespaces"
+                        placeholder="Password"
+                        type="password"
+                        onChange={handleChange}
+                    />
+                    <FormControl
+                        name="email"
+                        pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$"
+                        alertMessage="Invalid email"
+                        hidden={!signUpOpen}
+                        required={signUpOpen}
+                        type="email"
+                        placeholder="Email"
+                        onChange={handleChange}
+                    />
+                    <FormControl
+                        onChange={handleChange}
+                        name="phone_number"
+                        hidden={!signUpOpen}
+                        pattern="^[\d\+\s]{5,20}$"
+                        variant="warning"
+                        alertMessage="Invalid phone number (optional field)"
+                        type="tel"
+                        placeholder="Phone number"
+                    />
+                    <CountriesSelect name="location" onChange={handleChange} hidden={!signUpOpen} />
+                    <CurrencySelect name="user_currency" onChange={handleChange} hidden={!signUpOpen} />
+                    <button className="btn btn-link text-left" type="button" onClick={() => setSignUpOpen(prev => !prev)}>
+                        {signUpOpen ? 'Already registered login!' : 'Not an account yet? sign up!'}
+                    </button>
+                    {signUpOpen ?
+                        <button className="btn btn-outline-primary w-100" onClick={signUp}>
+                            Sign up
+                        </button>
+                        :
+                        <button className="btn btn-info w-100" onClick={login}>
+                            Login
+                        </button>
+                    }
+                </form>
+            </div>
+        }
+    </>
+    );
 }
 
 export default Login;
